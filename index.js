5afa905dbbdfdf70af105595145b9f360df61091
savonnur <74478757+savonnur@users.noreply.github.com>
2021-03-17 23:01:25 -0400

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+var url = 'https://api.exchangeratesapi.io/latest?base=';
+
+async function convert(){
+  var base = document.getElementById('Base').value;
+  var out = document.getElementById('Output').value;
+  var amount = document.getElementById('Amount').value;
+  var finalValue;
+
+  let response = await fetch(`${url}${base}`,{method: 'GET'})
+  let data = await response.json()
+
+  finalValue = data.rates
+
+  finalValue = finalValue[out]*Number(amount);
+  var result = `${finalValue} ${out}`;
+  document.getElementById('Converted').innerText = result;
+}
